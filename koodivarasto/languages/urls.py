dba74ff936acbe4bc7cbd2842af1cacd408e740d
ClausHofman <46158@edu.raseko.fi>
2023-02-15 15:28:09 +0200

    small additions, make development branch

--- koodivarasto/languages/urls.py
+++ koodivarasto/languages/urls.py
@@ -6,5 +6,6 @@ app_name = "languages"
 
 urlpatterns = [
     path('list/', views.listLanguages, name="language-list"),
-    path('language/<str:pk>/', views.viewSingleLanguage, name='view-language')
+    path('language/<str:pk>/', views.viewSingleLanguage, name='view-language'),
+    path('list-examples/', views.listExamples, name='language-examples'),
 ]
--- koodivarasto/languages/views.py
+++ koodivarasto/languages/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from .models import Language
+from .models import Language, LanguageExample
 
 def languagesIndex(request):
     return HttpResponse("Hello, world. You're at the languages index.")
@@ -13,4 +13,12 @@ def listLanguages(request):
 
 def viewSingleLanguage(request, pk):
     viewSingleLanguageObj = Language.objects.get(id=pk)
+
     return render(request, 'languages/view-language.html', {'object':viewSingleLanguageObj})
+
+def listExamples(request):
+    example = LanguageExample.objects.all()
+    context = {'example':example}
+    print(context)
+
+    return render(request, 'languages/language-examples.html', context)
