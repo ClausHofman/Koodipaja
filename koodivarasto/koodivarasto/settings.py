a9d1461169c827051224906381929052ea7d075c
ClausHofman <46158@edu.raseko.fi>
2023-02-17 12:34:45 +0200

    startapp tktutkija

--- koodivarasto/koodivarasto/settings.py
+++ koodivarasto/koodivarasto/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'tktutkija.apps.TktutkijaConfig',
     'testing.apps.TestingConfig',
     'frameworks.apps.FrameworksConfig',
     'users.apps.UsersConfig',
