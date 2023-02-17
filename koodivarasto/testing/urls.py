1dc7dd89a932d00ad1226dfed07a3140f1fa332f
ClausHofman <46158@edu.raseko.fi>
2023-02-17 12:13:00 +0200

    some testing app stuff

--- koodivarasto/testing/urls.py
+++ koodivarasto/testing/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-
 from . import views
 
 app_name = "testing"
 
 urlpatterns = [
-    path('testing/', views.testingHomepage, name='testing')
+    path('testing/', views.testingHomepage, name='testing'),
+    path('viewtest1/<str:pk>/', views.viewTest1, name='viewtest1'),
+    path('viewtest2/<str:pk>/', views.viewTest2, name='viewtest2'),
+
+
+    path('test_languages/', views.test_listLanguages, name='test_languages'),
 ]
