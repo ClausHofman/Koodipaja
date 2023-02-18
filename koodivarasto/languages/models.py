340ee12a637b67bbe6337380cb1a5e38a548ff5d
ClausHofman <46158@edu.raseko.fi>
2023-02-18 17:45:15 +0200

    language app Tag model

--- koodivarasto/languages/models.py
+++ koodivarasto/languages/models.py
@@ -18,6 +18,7 @@ class LanguageExample(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
     created = models.DateTimeField('created')
+    tags = models.ManyToManyField('Tag', blank=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
 
     def __str__(self):
@@ -25,3 +26,13 @@ class LanguageExample(models.Model):
 
     # class Meta:
     #     verbose_name = 'asdf'
+
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.name
