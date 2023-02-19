f733b9585cc0ce52515756a35a83d419da71d0cf
ClausHofman <46158@edu.raseko.fi>
2023-02-19 06:20:37 +0200

    mostly visual changes and some css

--- todo.py
+++ todo.py
@@ -1,3 +1,6 @@
 # NOTE: sitten kun tietokannassa on jotain muuta kuin testidataa, niin voisi käyttää Postgresql.
+# NOTE: ehkä pitää myös toisen kehityshaaran missä on käytössä sqlite.
 
 # TODO: frameworks appi -> django-query esimerkkejä?
+
+# TODO: implement some sort of back button etc for easier navigation.
