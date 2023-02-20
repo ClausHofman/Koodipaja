5d5589a3437057858ee89642e288fc0bb465b7b6
ClausHofman <46158@edu.raseko.fi>
2023-02-20 17:23:49 +0200

    project rename test

--- koodivarasto/koodipaja/wsgi.py
+++ koodivarasto/koodipaja/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for koodivarasto project.
+WSGI config for koodipaja project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'koodivarasto.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'koodipaja.settings')
 
 application = get_wsgi_application()
