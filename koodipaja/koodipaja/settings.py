a77187e460bbfc6fc47cc6631f908260cae39c36
ClausHofman <46158@edu.raseko.fi>
2023-02-20 23:41:27 +0200

    file was in update-index --skip temporarily

--- koodipaja/koodipaja/settings.py
+++ koodipaja/koodipaja/settings.py
@@ -84,11 +84,21 @@ WSGI_APPLICATION = 'koodipaja.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'koodipaja',
+        'USER': 'postgres',
+        'PASSWORD': 'Q2werty',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
