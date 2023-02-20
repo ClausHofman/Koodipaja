7f20d25a97c5a86153a20b35306b3747bf838429
ClausHofman <46158@edu.raseko.fi>
2023-02-20 23:53:55 +0200

    settings.py for this branch

--- koodipaja/koodipaja/settings.py
+++ koodipaja/koodipaja/settings.py
@@ -82,23 +82,23 @@ WSGI_APPLICATION = 'koodipaja.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'koodipaja',
-        'USER': 'postgres',
-        'PASSWORD': 'Q2werty',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'koodipaja',
+#         'USER': 'postgres',
+#         'PASSWORD': 'Q2werty',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
