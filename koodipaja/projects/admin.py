bc3b5a36f94815660d46e5b7bbb2c07503cc9839
ClausHofman <46158@edu.raseko.fi>
2023-02-27 10:47:59 +0200

    check if article has a title

--- koodipaja/projects/admin.py
+++ koodipaja/projects/admin.py
@@ -1,13 +1,21 @@
 from django.contrib import admin
 from .models import (Project, ProjectPage, ProjectArticle, ProjectTag, ProjectPageTag, ProjectArticleTag,
-                    ProjectTodo, ProjectTodoTag)
+                     ProjectTodo, ProjectTodoTag)
 
 # Register your models here.
 admin.site.register(Project)
 admin.site.register(ProjectTodo)
 admin.site.register(ProjectPage)
-admin.site.register(ProjectArticle)
 admin.site.register(ProjectTag)
 admin.site.register(ProjectTodoTag)
 admin.site.register(ProjectPageTag)
 admin.site.register(ProjectArticleTag)
+
+
+@admin.register(ProjectArticle)
+class ProjectArticleAdmin(admin.ModelAdmin):
+    list_display = ["created", "title"]
+    search_fields = ['body']
+
+    class Meta:
+        list_filter = ['-created']
--- koodipaja/projects/models.py
+++ koodipaja/projects/models.py
@@ -2,16 +2,20 @@ from django.db import models
 import uuid
 from users.models import Profile
 
+
 class Project(models.Model):
-    owner = models.ForeignKey(Profile, null=True, blank=True, on_delete=models.SET_NULL)
+    owner = models.ForeignKey(
+        Profile, null=True, blank=True, on_delete=models.SET_NULL)
     title = models.CharField(max_length=200)
     body = models.TextField(null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     tags = models.ManyToManyField('ProjectTag', blank=True)
-    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
 
     def __str__(self):
         return self.title
+
 
 class ProjectTodo(models.Model):
     owner = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True)
@@ -20,36 +24,43 @@ class ProjectTodo(models.Model):
     body = models.TextField(null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     tags = models.ManyToManyField('ProjectTodoTag', blank=True)
-    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
-
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
 
     def __str__(self):
         return self.title
 
+
 class ProjectPage(models.Model):
-    owner = models.ForeignKey(Profile, null=True, blank=True, on_delete=models.SET_NULL)
+    owner = models.ForeignKey(
+        Profile, null=True, blank=True, on_delete=models.SET_NULL)
     project = models.ForeignKey(Project, on_delete=models.SET_NULL, null=True)
     title = models.CharField(max_length=500)
     page_number = models.IntegerField()
     created = models.DateTimeField(auto_now_add=True)
     tags = models.ManyToManyField('ProjectPageTag', blank=True)
-    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
 
     def __str__(self):
         return self.title
+
 
 class ProjectArticle(models.Model):
     owner = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True)
     project = models.ForeignKey(Project, on_delete=models.SET_NULL, null=True)
-    project_page = models.ForeignKey(ProjectPage, on_delete=models.SET_NULL, null=True)
-    title = models.CharField(max_length=200)
+    project_page = models.ForeignKey(
+        ProjectPage, on_delete=models.SET_NULL, null=True)
+    title = models.CharField(max_length=200, null=True, blank=True)
     body = models.TextField(null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     tags = models.ManyToManyField('ProjectArticleTag', blank=True)
-    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
 
     def __str__(self):
-        return self.title
+        return self.title or ''
+
 
 class ProjectTag(models.Model):
     name = models.CharField(max_length=200)
@@ -59,6 +70,7 @@ class ProjectTag(models.Model):
     def __str__(self):
         return self.name
 
+
 class ProjectTodoTag(models.Model):
     name = models.CharField(max_length=200)
     created = models.DateTimeField(auto_now_add=True)
@@ -67,6 +79,7 @@ class ProjectTodoTag(models.Model):
     def __str__(self):
         return self.name
 
+
 class ProjectPageTag(models.Model):
     name = models.CharField(max_length=200)
     created = models.DateTimeField(auto_now_add=True)
@@ -74,6 +87,7 @@ class ProjectPageTag(models.Model):
 
     def __str__(self):
         return self.name
+
 
 class ProjectArticleTag(models.Model):
     name = models.CharField(max_length=200)
