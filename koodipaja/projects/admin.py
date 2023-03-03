71e45f86ca4ebf12c468995d39980a24c5433f38
ClausHofman <46158@edu.raseko.fi>
2023-03-03 09:13:16 +0200

    ProjectPage list_display

--- koodipaja/projects/admin.py
+++ koodipaja/projects/admin.py
@@ -6,17 +6,25 @@ from .models import (Project, ProjectPage, ProjectArticle, ProjectTag, ProjectPa
 # Register your models here.
 admin.site.register(Project)
 admin.site.register(ProjectTodo)
-admin.site.register(ProjectPage)
 admin.site.register(ProjectTag)
 admin.site.register(ProjectTodoTag)
 admin.site.register(ProjectPageTag)
 admin.site.register(ProjectArticleTag)
 
 
+@admin.register(ProjectPage)
+class ProjectPageAdmin(admin.ModelAdmin):
+    list_display = ["created", "title"]
+    search_fields = ['title']
+
+    class Meta:
+        list_filter = ["-created"]
+
+
 @admin.register(ProjectArticle)
 class ProjectArticleAdmin(admin.ModelAdmin):
     list_display = ["created", "project_page", "title"]
-    search_fields = ['body']
+    search_fields = ['body', 'title']
 
     class Meta:
         list_filter = ["-created"]
