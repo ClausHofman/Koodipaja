8ec56960980158a5a9f265ac5ef50043ebd3965e
ClausHofman <46158@edu.raseko.fi>
2023-04-28 15:02:00 +0300

    profile creation signal working

---
+++ koodipaja/users/signals.py
@@ -0,0 +1,61 @@
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+
+from django.contrib.auth.models import User
+from .models import Profile
+
+# imports for email
+from django.core.mail import send_mail
+# from email_passu import app_email
+
+
+# @receiver(post_save, sender=Profile) # signal with decorator
+def createProfile(sender, instance, created, **kwargs):
+    # new user created -> create a profile for that user
+    if created:
+        user = instance
+        profile = Profile.objects.create(
+            user=user,
+            username=user.username,
+            email=user.email,
+            name=user.first_name
+        )
+
+
+def updateUser(sender, instance, created, **kwargs):
+    # take advantage of OnetoOne-relationship
+    # profile = instance.user
+
+    if created == False:  # important
+        profile = instance
+        user = profile.user
+        user.first_name = profile.name
+        user.username = profile.username
+        user.email = profile.email
+        user.save()
+
+
+# auto delete a user on profile deletion
+def deleteUser(sender, instance, **kwargs):
+    # had to use try-except, or the delete wouldn't work
+    try:
+        user = instance.user
+        user.delete()
+    except:
+        pass
+
+
+# auto create profile on user creation
+post_save.connect(createProfile, sender=User)
+
+# if profile is updated, trigger updateUser
+post_save.connect(updateUser, sender=Profile)
+
+# auto delete a user on profile deletion
+post_delete.connect(deleteUser, sender=Profile)
+
+
+# def profileUpdated(sender, instance, created, **kwargs):
+#     print('Profile Saved!')
+#     print('Instance:', instance)
+#     print('CREATED:', created)
