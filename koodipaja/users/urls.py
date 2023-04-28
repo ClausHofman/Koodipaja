18660df1c70a544023b697aac0e06ef0dea431fd
ClausHofman <46158@edu.raseko.fi>
2023-04-28 10:35:47 +0300

    start: try to make signup, login, logout etc.

--- koodipaja/users/urls.py
+++ koodipaja/users/urls.py
@@ -4,4 +4,7 @@ from . import views
 app_name = 'users'
 urlpatterns = [
     path('profile/<str:pk>/', views.userProfile, name="user-profile"),
+    path('login/', views.loginUser, name="login"),
+    path('logout/', views.logoutUser, name="logout"),
+    path('register/', views.registerUser, name="register"),
 ]
